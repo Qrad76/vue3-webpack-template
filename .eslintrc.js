a202ee726efe5a2d63cc4720f1596c76b1f20211
Qrad76 <qrad76@gmail.com>
2021-09-27 16:09:30 +0900

    Vue.js-Webpack 템플릿 만들기

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extend: [
+    //vue
+    // "plugin:vue/vue3-essential",
+    "plugin:vue/vue3-strongly-recommended",
+    // "plugin:vue/vue3-recommended",
+
+    //js
+    "eslint:recommended",
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+
+  },
+};
